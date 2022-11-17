af51be358a1ec724be511302cef1d9a841756bc6
Jacob Tipp <jacobtipp@gmail.com>
2022-11-16 19:12:08 -0800

    feat: updated API
            BREAKING CHANGE: State will now have a new property named status to identify its
            finite state, immer will now be integrated into the library and is now a
            hard dependency. State will no longer use static factory methods.
            Instantiating a state instance will set it to intial status. New state
            instances can be created using prototype methods on a state instance [ready, failed,
            loading]. These 3 methods will return a new immutable state instance.

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,90 +1,67 @@
-import { isBlocStateInstance } from "../src/state"
+import { BlocStateStatus, isBlocStateInstance } from "../src"
 import { CounterState } from "./helpers/counter/counter.state"
 
 describe("BlocState", () => {
   let state: CounterState
 
-  describe("BlocState.payload.initializing", () => {
-    it("should set state to initial", () => {
-      state = CounterState.init(0)
-      expect(state).toBeInstanceOf(CounterState)
-      expect(state.payload.initial).toBe(true)
-      expect(state.payload.hasData).toBe(true)
-      expect(state.payload.data).toBe(0)
-      expect(state.payload.loading).toBe(false)
-      expect(state.payload.hasError).toBe(false)
-      expect(state.payload.error).toBeUndefined()
-      expect(state.payload.isFailure).toBe(false)
-    })
+  beforeEach(() => {
+    state = new CounterState()
   })
 
-  describe("BlocState.loading", () => {
-    it("should set state to loading with no data", () => {
-      state = CounterState.loading()
-      expect(state.payload.initial).toBe(false)
-      expect(state.payload.hasData).toBe(false)
-      expect(state.payload.data).toBeUndefined()
-      expect(state.payload.loading).toBe(true)
-      expect(state.payload.hasError).toBe(false)
-      expect(state.payload.error).toBeUndefined()
-      expect(state.payload.isFailure).toBe(false)
-    })
-
-    it("should set state to loading with data", () => {
-      state = CounterState.loading(0)
-      expect(state.payload.initial).toBe(false)
-      expect(state.payload.hasData).toBe(true)
-      expect(state.payload.data).toBe(0)
-      expect(state.payload.loading).toBe(true)
-      expect(state.payload.hasError).toBe(false)
-      expect(state.payload.error).toBeUndefined()
-      expect(state.payload.isFailure).toBe(false)
+  describe("BlocState creation", () => {
+    it("should set state to initial", () => {
+      expect(state).toBeInstanceOf(CounterState)
+      expect(state.status).toBe(BlocStateStatus.initial)
+      expect(state.data).toBe(0)
     })
   })
 
   describe("BlocState.ready", () => {
-    it("should set state to ready with data", () => {
-      state = CounterState.ready(0)
-      expect(state.payload.initial).toBe(false)
-      expect(state.payload.data).toBe(0)
-      expect(state.payload.loading).toBe(false)
-      expect(state.payload.hasError).toBe(false)
-      expect(state.payload.error).toBeUndefined()
-      expect(state.payload.hasData).toBe(true)
-      expect(state.payload.isFailure).toBe(false)
+    it("should set state to ready", () => {
+      const readyState = state.ready((data) => data + 3)
+      expect(readyState).toBeInstanceOf(CounterState)
+      expect(readyState.status).toBe(BlocStateStatus.ready)
+      expect(readyState.data).toBe(3)
+      expect(readyState === state).toBe(false)
+
+      const readyState2 = readyState.ready()
+      expect(readyState2).toBeInstanceOf(CounterState)
+      expect(readyState2.status).toBe(BlocStateStatus.ready)
+      expect(readyState2.data).toBe(3)
+      expect(readyState2 === readyState).toBe(false)
+    })
+  })
+
+  describe("BlocState.loading", () => {
+    it("should set state to loading", () => {
+      const loading = state.loading()
+      expect(loading).toBeInstanceOf(CounterState)
+      expect(loading.status).toBe(BlocStateStatus.loading)
+      expect(loading.data).toBe(0)
+      expect(loading === state).toBe(false)
     })
   })
 
   describe("BlocState.failed", () => {
-    it("should set state to failed with error", () => {
-      const error = new Error("Invalid constructor arguments for Bloc State.")
-      state = CounterState.failed(error)
-      expect(state.payload.initial).toBe(false)
-      expect(state.payload.data).toBeUndefined()
-      expect(state.payload.loading).toBe(false)
-      expect(state.payload.hasError).toBe(true)
-      expect(state.payload.hasData).toBe(false)
-      expect(state.payload.error?.message).toBe(
-        "Invalid constructor arguments for Bloc State.",
-      )
-      expect(state.payload.isFailure).toBe(true)
-    })
+    it("should set state to failed", () => {
+      const failed = state.failed(new Error("operation has failed"))
+      expect(failed).toBeInstanceOf(CounterState)
+      expect(failed.error).toBeDefined()
+      expect(failed.error?.message).toBe("operation has failed")
+      expect(failed.data).toBe(state.data)
+      expect(failed === state).toBe(false)
 
-    it("should set state to failed without error", () => {
-      state = CounterState.failed()
-      expect(state.payload.initial).toBe(false)
-      expect(state.payload.data).toBeUndefined()
-      expect(state.payload.loading).toBe(false)
-      expect(state.payload.hasError).toBe(false)
-      expect(state.payload.hasData).toBe(false)
-      expect(state.payload.error).toBeUndefined()
-      expect(state.payload.isFailure).toBe(true)
+      const failed2 = failed.failed()
+      expect(failed2).toBeInstanceOf(CounterState)
+      expect(failed2.error).toBeUndefined()
+      expect(failed2.error?.message).toBeUndefined()
+      expect(failed2.data).toBe(failed.data)
+      expect(failed2 === failed).toBe(false)
     })
   })
 
   describe("isBlocStateInstance function", () => {
     it("should return true only if an object is an instance of BlocState", () => {
-      state = CounterState.init(0)
       class Test {}
       const test = new Test()
       expect(isBlocStateInstance(state)).toBe(true)
