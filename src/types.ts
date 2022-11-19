cbdc5f7e767238b09131ad0146db5440ed3d392e
Jacob Tipp <jacobtipp@gmail.com>
2022-11-18 17:27:07 -0800

    feat: made changes to handle encapsulation properly

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,1 @@
-import { BlocState } from "./state"
-
-export type BlocStateDataType<T> = T extends BlocState<infer U> ? U : T
-
-export type BlocStatePayload<T, E extends Error = Error> = {
-  status?: BlocStateStatus
-  data: T
-  error?: E
-}
-
-export enum BlocStateStatus {
-  initial,
-  loading,
-  ready,
-  failed,
-}
+export type BlocStateStatus = "initial" | "loading" | "ready" | "failed"
